5d7135894f623587ef5fc088eefa7d38e1935ec5
Danidev16 <reginodaniel78@gmail.com>
2021-02-11 17:59:09 -0500

    version 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+declare var Jquery:any;
+declare var $:any;
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  title = 'amazon-mod';
+}
